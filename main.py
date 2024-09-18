2bfae8630576bc71e5805c6c50907a307651fb6e
Gabriel Foriel-Fusier <forielfusierg@educ.college-montreal.qc.ca>
2024-09-18 13:42:47 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+    Nom: Gabriel Foriel Fusier
+    Groupe:401
+    Descritpion: Programme qui compte le nombre de mot dans une chaine de caractère
+"""
+
+sentence = "Je suis riche mon petit canard"
+sentence = sentence.split()
+print(len(sentence))
